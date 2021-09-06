2c6d438ec357290f7806b05b54bc3a8f5a1ef136
RathodDeven <devenrathodrd@gmail.com>
2021-09-06 12:51:02 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkrs6N8Q3JKL59Zkn-qp9zm8Z7LniZ5Rs",
+    authDomain: "chat-web-app-2bdc3.firebaseapp.com",
+    databaseURL: "https://chat-web-app-2bdc3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-web-app-2bdc3",
+    storageBucket: "chat-web-app-2bdc3.appspot.com",
+    messagingSenderId: "968501083359",
+    appId: "1:968501083359:web:c772eba7959a5f0563bd6a"
+  };
+
+
+const app = firebase.initializeApp(firebaseConfig);
+  
